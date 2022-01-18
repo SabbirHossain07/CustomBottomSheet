4ec1b2aba501421bb8dce6d6d9ca227eab6ae6a3
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-01-18 14:23:33 +0600

    Initial Commit

---
+++ CustomBottomSheet/CustomBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBottomSheetApp.swift
+//  CustomBottomSheet
+//
+//  Created by Sopnil Sohan on 18/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
