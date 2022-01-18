41d9793b6215fcf5e0e5aeb68a77405b28b1ad7b
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-01-18 20:19:23 +0600

    Done!

---
+++ CustomBottomSheet/CustomCorner.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCorner.swift
+//  CustomBottomSheet
+//
+//  Created by Sopnil Sohan on 18/1/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomCorner: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+}
